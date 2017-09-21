0de04cbfd4e12578801cc2b71752efb073316aa3
irom77 <irek.romaniuk@gmail.com>
2017-09-21 07:52:00 -0400

    some comments removed

--- imports/ui/ShieldsList.js
+++ imports/ui/ShieldsList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 import Griddle, { plugins, RowDefinition, ColumnDefinition } from 'griddle-react';
-// import '../client/styles/components/griddle.css'
-// import { Session } from 'meteor/session';
-// import FlipMove from 'react-flip-move';
 
 import { Shields } from '../api/shields';
 import ShieldsListItem from './ShieldsListItem';
@@ -28,19 +25,7 @@ export default class ShieldsList extends React.Component {
     console.log('componentWillUnmount ShieldsList');
     this.shieldsTracker.stop();
   }
-  /*renderShieldsListItems() {
-    if (this.state.shields.length === 0) {
-      return (
-        <div className="item">
-          <p className="item__status-message">No Shields Found</p>
-        </div>
-      );
-    }
-
-    return this.state.shields.map((shield) => {
-      return <ShieldsListItem key={shield._id} {...shield}/>;
-    });
-  }*/
+ 
   render() {
     return (
         <div>         
